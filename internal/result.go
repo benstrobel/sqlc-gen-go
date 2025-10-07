8ddd6c37cce2042d3fd91e382abe6b4e45010be7
Ben Strobel <benedikt.strobel@tum.de>
2025-10-07 19:33:25 +0200

    Update package path

--- internal/result.go
+++ internal/result.go
@@ -6,11 +6,11 @@ import (
 	"sort"
 	"strings"
 
-	"github.com/sqlc-dev/sqlc-gen-go/internal/opts"
-	"github.com/sqlc-dev/plugin-sdk-go/sdk"
-	"github.com/sqlc-dev/sqlc-gen-go/internal/inflection"
+	"github.com/benstrobel/sqlc-gen-go/internal/inflection"
+	"github.com/benstrobel/sqlc-gen-go/internal/opts"
 	"github.com/sqlc-dev/plugin-sdk-go/metadata"
 	"github.com/sqlc-dev/plugin-sdk-go/plugin"
+	"github.com/sqlc-dev/plugin-sdk-go/sdk"
 )
 
 func buildEnums(req *plugin.GenerateRequest, options *opts.Options) []Enum {
